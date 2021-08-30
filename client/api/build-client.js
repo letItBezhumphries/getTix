17eca1c33e4e6539ed21d1f871034f4117f6a43a
Eric Humphries <ezhumphries@gmail.com>
2021-08-30 16:12:49 -0700

    changed the cookie setting for middleware and changed the hostname to the domain name created

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,18 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default ({ req }) => {
-  if (typeof window === 'undefined') {
+  if (typeof window === "undefined") {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: "http://www.ez-ticketing-prod-app.xyz/",
       headers: req.headers,
     });
   } else {
     // We must be on the browser
     return axios.create({
-      baseUrl: '/',
+      baseUrl: "/",
     });
   }
 };
